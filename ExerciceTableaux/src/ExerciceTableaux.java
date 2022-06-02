183f48607107c8e3794cf47d68b7325f9e3c9f54
Normandin <95148064+daveonduty006@users.noreply.github.com>
2022-06-02 16:59:36 -0400

    continue ExerciceTableaux

--- ExerciceTableaux/src/ExerciceTableaux.java
+++ ExerciceTableaux/src/ExerciceTableaux.java
@@ -14,36 +14,57 @@ public class ExerciceTableaux {
 
 	public static void main(String[] args) {
 		final int NB_PROD= 8;
-		int tabNoProd[]=      {234,  125, 657, 987,  213, 934,  678,  776};
-		double tabPrixProd[]= {45.99,9.50,5.75,12.35,9.75,87.45,56.99,76.56};
-		
-		
+		int tabQteTotale[]= new int[8];
+		int tabNoProd[]=  {234,  125, 657, 987,  213, 934,  678,  776};
+		double tabPrix[]= {45.99,9.50,5.75,12.35,9.75,87.45,56.99,76.56};		
 		//
-		/*traiterLesClients( );
-		 *afficherResultats( );
-		 */
+		traiterLesClients(tabNoProd, tabPrix, tabQteTotale, NB_PROD);
+		afficherResultats(tabNoProd, tabQteTotale, NB_PROD);
 		//
 		System.exit(0);
-	}
+	} // fin de la méthode main
 	
 	static void traiterLesClients(int tabNoProd[], double tabPrix[],
-			int tabQteTotale[], int nbProd) {
-		DecimalFormat cash = new DecimalFormat("0.00 $");
+			                      int tabQteTotale[], int NB_PROD) {
+		DecimalFormat cash= new DecimalFormat("0.00 $");
 		int numero, qte, posiProd; // position du numéro dans le tableau tabNoProd
 		double cout;
 		char reponse;
 
 		do {
-			numero = Integer.parseInt(JOptionPane.showInputDialog(
-					"Entrez le numéro du produit à acheter :"));
-			qte = Integer.parseInt(JOptionPane.showInputDialog(
-					"Entrez la quantité désirée :"));
-			if (posiProd != -1) {
-				
+			numero= Integer.parseInt(JOptionPane.showInputDialog(
+					"Entrez le numero du produit a acheter :"));
+			qte= Integer.parseInt(JOptionPane.showInputDialog(
+					"Entrez la quantite desiree :"));
+			for (int i=0; tabNoProd[i] != numero; i++) {
+				if (tabNoProd[i] ==)
 			}
+			if (posiProd != -1) {
+				cout; 
+				JOptionPane.showMessageDialog(null,
+					"Le coût de cet achat est de " + cash.format(cout));
+			}else {
+				JOptionPane.showMessageDialog(null, "No de produit erroné");
+				reponse = JOptionPane.showInputDialog(
+					"Avez-vous un autre client à traiter O/N ?").charAt(0);
+				reponse = Character.toUpperCase(reponse);
+		}while (reponse == 'O');		
+	} // fin de la méthode traiterLesClients
 
+	static int rechercher(int tab[], int nbEl, int valeurCherchee) {
+		int posi = -1;
+		boolean trouve = false;
+		for (int i = 0; i < nbEl && !trouve; i++)
+		if (tab[i] == valeurCherchee) {
+			posi = i;
+			trouve = true;
 		}
+		return posi;
+	} // fin de la méthode rechercher
 		
-	}
+	static void afficherResultats(int tabNoProd[], int tabQteTotale[],
+								  int NB_PROD) {
+		
+	} // fin de la méthode afficherResultats
 
-}
+} // fin de la classe
