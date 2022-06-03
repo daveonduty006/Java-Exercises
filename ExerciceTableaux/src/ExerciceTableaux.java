7d8b75cabfdd492de0623f15b33d65fcde809ff8
Normandin <95148064+daveonduty006@users.noreply.github.com>
2022-06-03 11:05:09 -0400

    finished ExerciceTableaux

--- ExerciceTableaux/src/ExerciceTableaux.java
+++ ExerciceTableaux/src/ExerciceTableaux.java
@@ -14,7 +14,7 @@ public class ExerciceTableaux {
 
 	public static void main(String[] args) {
 		final int NB_PROD= 8;
-		int tabQteTotale[]= new int[8];
+		int tabQteTotale[]= new int[NB_PROD];
 		int tabNoProd[]=  {234,  125, 657, 987,  213, 934,  678,  776};
 		double tabPrix[]= {45.99,9.50,5.75,12.35,9.75,87.45,56.99,76.56};		
 		//
@@ -22,49 +22,66 @@ public class ExerciceTableaux {
 		afficherResultats(tabNoProd, tabQteTotale, NB_PROD);
 		//
 		System.exit(0);
-	} // fin de la méthode main
+	} 
 	
 	static void traiterLesClients(int tabNoProd[], double tabPrix[],
-			                      int tabQteTotale[], int NB_PROD) {
+			                      int tabQteTotale[], int nbProd) {
 		DecimalFormat cash= new DecimalFormat("0.00 $");
-		int numero, qte, posiProd; // position du numéro dans le tableau tabNoProd
+		int numero, qte, posiProd; 
 		double cout;
 		char reponse;
-
+		//
 		do {
 			numero= Integer.parseInt(JOptionPane.showInputDialog(
-					"Entrez le numero du produit a acheter :"));
+				"Entrez le numero du produit a acheter :"));
 			qte= Integer.parseInt(JOptionPane.showInputDialog(
-					"Entrez la quantite desiree :"));
-			for (int i=0; tabNoProd[i] != numero; i++) {
-				if (tabNoProd[i] ==)
-			}
+				"Entrez la quantite desiree :"));
+			//
+			posiProd= rechercher(tabNoProd, nbProd, numero);
+			//
 			if (posiProd != -1) {
-				cout; 
+				tabQteTotale[posiProd]+= qte;
+				cout= tabPrix[posiProd] * qte;
 				JOptionPane.showMessageDialog(null,
-					"Le coût de cet achat est de " + cash.format(cout));
+					"Le cout de cet achat est de " + cash.format(cout));
 			}else {
-				JOptionPane.showMessageDialog(null, "No de produit erroné");
-				reponse = JOptionPane.showInputDialog(
-					"Avez-vous un autre client à traiter O/N ?").charAt(0);
-				reponse = Character.toUpperCase(reponse);
-		}while (reponse == 'O');		
-	} // fin de la méthode traiterLesClients
+				JOptionPane.showMessageDialog(null, "No de produit errone");
+			}
+			reponse = JOptionPane.showInputDialog(
+				"Avez-vous un autre client a traiter O/N ?").charAt(0);
+			reponse = Character.toUpperCase(reponse);
+		}while (reponse == 'O');
+		//
+	} 
 
-	static int rechercher(int tab[], int nbEl, int valeurCherchee) {
+	static int rechercher(int tabNoProd[], int nbProd, int numeroCherche) {
 		int posi = -1;
 		boolean trouve = false;
-		for (int i = 0; i < nbEl && !trouve; i++)
-		if (tab[i] == valeurCherchee) {
-			posi = i;
-			trouve = true;
+		//
+		for (int i=0; i < nbProd && !trouve; i++) {
+			if (tabNoProd[i] == numeroCherche) {
+				posi = i;
+				trouve = true;
+			}
 		}
+		//
 		return posi;
-	} // fin de la méthode rechercher
-		
+	} 
+			
 	static void afficherResultats(int tabNoProd[], int tabQteTotale[],
-								  int NB_PROD) {
-		
-	} // fin de la méthode afficherResultats
+								  int nbProd) {
+		JTextArea sortie = new JTextArea();
+		//
+		sortie.append("Numero du\t\tQuantite\n");
+		sortie.append("produit\t\ttotale\n\n");
+		for (int i=0; i < nbProd; i++) {
+			sortie.append(tabNoProd[i]+"\t\t"+tabQteTotale[i]+"\n");
+		}
+		//
+		JOptionPane.showMessageDialog(null, sortie, "RESULTATS DE LA JOURNEE", 
+			JOptionPane.PLAIN_MESSAGE);
+		//
+	} 
 
-} // fin de la classe
+} 
+
