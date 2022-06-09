13329f309b734689a329089fb7e2eabc47324e0c
daveonduty006 <dvdnormans@hotmail.com>
2022-06-09 11:10:30 -0400

    finished first part of Lab1_Partie2

--- Labo1_Partie2/src/TestVin.java
+++ Labo1_Partie2/src/TestVin.java
@@ -1,12 +1,3 @@
-/*
- * Créer les trois vins et les afficher
- * Ajouter 2$ au prix du premier vin
- * Modifier le prix et l'origine du deuxième vin
- * Modifier le nom et le type du troisième vin
- * Donner au troisième vin la même origine que le premier
- * Créer un quatrième vin
- * Afficher tous les vins
- */
 
 import javax.swing.*;
 import java.util.ArrayList;
@@ -14,30 +5,38 @@ import java.util.ArrayList;
 public class TestVin {
 	
 	static ArrayList<Vin> tabVins;
-	static JTextArea sortie;
+	static JTextArea sortie= new JTextArea();
 
 	public static void main(String[] args) {
-		instancierVins();
-		afficherVins();
-		System.exit(0);
-	}
+		instancieVinsInitials();
+		prepareAffichage();
+		// MODIFICATION ET AJOUT DE VINS 
+		tabVins.get(0).setPrix(tabVins.get(0).getPrix() + 2);
+		tabVins.get(1).setPrix(tabVins.get(1).getPrix() + 8.5);
+		tabVins.get(1).setOrigine("Italie");
+		tabVins.get(2).setNom("Vino verde");
+		tabVins.get(2).setType(2);
+		tabVins.get(2).setOrigine(tabVins.get(0).getOrigine());
+		tabVins.add(new Vin("L'érablière", "Québec", 15));
+		//
+		sortie.append("\n");
+		prepareAffichage();
+		JOptionPane.showMessageDialog(null, sortie, "Résultats obtenus",
+				JOptionPane.PLAIN_MESSAGE);	
+		//
+		System.exit(0);}
 	
-	public static void instancierVins() {
+	public static void instancieVinsInitials() {
 		tabVins= new ArrayList<>();
 		tabVins.add(new Vin("MiamMiam", 2, "Espagne", 8.95));
 		tabVins.add(new Vin("Délicieux", "France", 14.5));
-		tabVins.add(new Vin("Mystère", 3, "Californie", 10));
-	}
+		tabVins.add(new Vin("Mystère", 3, "Californie", 10));}
 	
-	public static void afficherVins() {
-		sortie= new JTextArea();
+	public static void prepareAffichage() {
 		sortie.append("Voici les "+Vin.nbVins+" vins\n");
 		sortie.append("Le prix total des vins est de "+
 			Vin.df.format(Vin.prixTotal)+"\n");
 		tabVins.forEach((unVin) -> sortie.append("              "+
-			unVin.toString()));
-		JOptionPane.showMessageDialog(null, sortie, "Résultats obtenus",
-			JOptionPane.PLAIN_MESSAGE);		
-	}
+			unVin.toString()));}
 
 }
