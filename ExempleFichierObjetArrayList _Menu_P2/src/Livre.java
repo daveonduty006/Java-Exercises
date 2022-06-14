2f052347f2697b5a53517c3d150d2e56d427c1a4
Normandin <95148064+daveonduty006@users.noreply.github.com>
2022-06-14 15:39:00 -0400

    misc commit

--- ExempleFichierObjetArrayList _Menu_P2/src/Livre.java
+++ ExempleFichierObjetArrayList _Menu_P2/src/Livre.java
@@ -9,28 +9,23 @@ public class Livre {
 	private String titre;
 	private int pages;
 	
-	//Constructeur par défault
+	//Constructeur par default
 	Livre(){
 		++nbLivres;
 	}
 	
-	//Constructeur par paramètres
+	//Constructeur par parametres
 	Livre(int num, String titre, int pages) {
-		this.setNum(num); //validation de données pour attributs d'instance num et pages
+		this.num = num;
 		this.titre = titre;
-		this.setPages(pages);
+		this.pages = pages;
 		++nbLivres;
-	}
-	
-	//Constructeur paramétré avec valeur de pages indéterminée
-	Livre(int num, String titre) {
-		this(num, titre, 0); //appelle constructeur à 3 paramètres avec une valeur de pages indéterminée
 	}
 	
 	//Constructeur de copie
 	Livre(Livre unLivre) {
 		this.num = unLivre.num;
-		this.titre = unLivre.titre; // Constructeur de copie, alternative: utiliser nomObjet.clone
+		this.titre = unLivre.titre; // Constructeur de copie, alternative: utiliser nomObject.clone
 		this.pages = unLivre.pages;
 		++nbLivres;
 	}
@@ -49,11 +44,11 @@ public class Livre {
 	}
 	
 	//Les mutateurs
-	public void setNum(int num) {
+	public void setNum(int nouveauNum) {
 		if (num > 0) {
-			this.num = num;
+			this.num = nouveauNum;
 		}else {
-			System.out.println("numéro de livre invalide!");
+			System.out.println("numero de livre invalide!");
 		}
 	}
 	
@@ -61,15 +56,15 @@ public class Livre {
 		this.titre = nouveauTitre;
 	}
 	
-	public void setPages(int pages) {
+	public void setPages(int nouveauPages) {
 		if (pages > 0) {
-			this.pages = pages;
+			this.pages = nouveauPages;
 		}else {
 			System.out.println("nombre de pages invalide");
 		}
 	}
 	
-	//Retourne le contenu d'un objet selon un format voulu
+	//Retourner le contenu d'un object selon un format voulu
 	public String toString() {
 		if (this.titre.length() > 17) {
 		return (this.num+"\t"+this.titre+"\t"+this.pages+"\n");	
--- ExempleFichierObjetTab/src/GestionLivres.java
+++ ExempleFichierObjetTab/src/GestionLivres.java
@@ -12,19 +12,6 @@ public class GestionLivres {
 	public static void main(String[] args) throws Exception {
 		chargerLivres();
 		afficherListeLivres();
-	   /*Livre Livre1 = new Livre();
-		*Livre Livre2 = new Livre(100, "Titanic", 400);
-		*Livre Livre3 = new Livre(Livre2);	
-		*biblio[0] = new Livre(200, "Le Soleil Vert", 350);
-		*biblio[1] = Livre2;
-		*biblio[2] = new Livre(biblio[0]);
-		*for (Livre unLivre: biblio) {
-		*	if (unLivre != null) {
-		*		System.out.println(unLivre);
-		*	}else {
-		*		break;
-		*	}
-		}*/
 	}
 
 	public static void chargerLivres() throws Exception {
@@ -50,19 +37,20 @@ public class GestionLivres {
 		}catch(Exception e) {
 			System.out.println("Un problème est arrivé lors du chargement du"+
 				" fichier");
-		}finally { //execute si erreur ou pas
+		}finally { //exécute si erreur ou pas
 			tmpLivres.close();
 		}
 	}
 	
 	public static void afficherListeLivres() {
 		sortie= new JTextArea();
-		int taille= Livre.nbLivres;
 		sortie.append("\tLISTE DES LIVRES\n\n");
 		sortie.append("NUMÉRO\tTITRE\t\tPAGES\n");
-		for (int i=0; i < taille; i++) {
-			sortie.append(biblio[i].toString());
-		}
+	    for (Livre unLivre: biblio) {
+	    	if (unLivre != null) {
+	    	sortie.append(unLivre.toString()); 
+	    	}
+	    }
 		JOptionPane.showMessageDialog(null, sortie, null, 
 			JOptionPane.PLAIN_MESSAGE);
 	}
