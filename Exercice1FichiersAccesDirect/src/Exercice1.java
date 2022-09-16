02d320c6076b8d4695f6c0cb7ee841331aca68b9
Normandin <95148064+daveonduty006@users.noreply.github.com>
2022-09-16 11:48:08 -0400

    (Exercice1FichiersAccesDirect) finished

--- Exercice1FichiersAccesDirect/src/Exercice1.java
+++ Exercice1FichiersAccesDirect/src/Exercice1.java
@@ -2,9 +2,9 @@ import java.io.*;
 
 public class Exercice1 {
 	
-	final static String FICHIER_BINAIRE= "src/donnees/employes.bin";
-	final static int TAILLE_ENREG= 36;
-	final static int INTERVAL_CLE= 100;
+	static final String FICHIER_BINAIRE= "src/donnees/employes.bin";
+	static final int TAILLE_ENREG= 36;
+	static final int INTERVAL_CLE= 100;
 	static int compteur;
 	static RandomAccessFile donnee;
 	static BufferedReader in;
@@ -19,11 +19,11 @@ public class Exercice1 {
                 try{
                     System.out.println("Menu");
                     System.out.println("====\n");
-                    System.out.println("1. Afficher les employés");
-                    System.out.println("2. Ajouter un employé");                 
-                    System.out.println("3. Modifier le salaire d'un employé");
+                    System.out.println("1. Afficher les employes");
+                    System.out.println("2. Ajouter un employe");                 
+                    System.out.println("3. Modifier le salaire d'un employe");
                     System.out.println("4. Calculer le salaire moyen"); 
-                    System.out.println("5. Rechercher un employé");  
+                    System.out.println("5. Rechercher un employe");  
                     System.out.println("6. Quitter");      
                     choix= Integer.parseInt(in.readLine());
                 }catch(NumberFormatException e)
@@ -49,7 +49,7 @@ public class Exercice1 {
 				donnee.close();
 				System.exit(0); 
 			}
-		}while(sortie != true); 
+		}while(!sortie); 
         donnee.close();
         System.exit(0);  
 	}
@@ -72,6 +72,7 @@ public class Exercice1 {
 		File fic= new File(FICHIER_BINAIRE);
 		if (fic.exists()) { 
 			donnee= new RandomAccessFile(fic, "rw");
+			compteur= (int) (donnee.length()/TAILLE_ENREG*INTERVAL_CLE);
 		}else{
 			donnee= new RandomAccessFile(fic, "rw");
 			compteur= INTERVAL_CLE;
@@ -91,7 +92,7 @@ public class Exercice1 {
 		String nom, prenom;
 		double sal;
 		donnee.seek(0);
-		for (int i=0; i < compteur/INTERVAL_CLE; i++) {
+		for (int i=0; i < donnee.length(); i++) {
             try{
 				num= donnee.readInt();
 				nom= donnee.readUTF();
@@ -111,11 +112,11 @@ public class Exercice1 {
         compteur+= INTERVAL_CLE;                  
         try{
         	donnee.writeInt(compteur);
-            System.out.println("Entrez le nom du nouvel employé: ");
+            System.out.println("Entrez le nom du nouvel employe: ");
             donnee.writeUTF(formatterString(in.readLine()));
-            System.out.println("Entrez le prénom du nouvel employé: ");            
+            System.out.println("Entrez le prénom du nouvel employe: ");            
             donnee.writeUTF(formatterString(in.readLine()));
-            System.out.println("Entrez le salaire du nouvel employé: ");
+            System.out.println("Entrez le salaire du nouvel employe: ");
             donnee.writeDouble(Double.parseDouble(in.readLine()));
         }catch(EOFException e)
             {}                     
@@ -127,7 +128,7 @@ public class Exercice1 {
         int num, numero;
         String nom, prenom;
         double sal;
-        System.out.println("Entrez le numéro de l'employé recherché: ");
+        System.out.println("Entrez le numero de l'employe recherche: ");
         do{
         	num= Integer.parseInt(in.readLine());
         }while(num%INTERVAL_CLE != 0 || num == 0);
@@ -144,133 +145,5 @@ public class Exercice1 {
         	{}
         System.out.println();
 	}
-	
-	
+		
 }
-
-
-/*    
-    do{       
-    	do{
-            try{
-                System.out.println("Menu");
-                System.out.println("====\n");
-                System.out.println("1. Afficher les donnees");
-                System.out.println("2. Calculer le salaire moyens");
-                System.out.println("3. Ajouter un employe");
-                System.out.println("4. Modifier le salaire d'un employe");
-                System.out.println("5. Quitter");      
-                choix= Integer.parseInt(in.readLine());
-            }catch(NumberFormatException e)
-                {}
-        }while(choix < 1 || choix > 5);
-    	
-    	switch(choix) {
-            case 1:
-                System.out.println();                     
-                donnee.seek(0);                    
-                for(int i=0; i < compteur; i++) {
-                    try{
-                    	System.out.print(donnee.readInt());
-                        System.out.print(donnee.readChar());
-                        System.out.print(donnee.readUTF());
-                        System.out.print(donnee.readChar());
-                        System.out.print(donnee.readUTF());
-                        System.out.print(donnee.readChar());
-                        System.out.print(donnee.readDouble());
-                        System.out.print(donnee.readChar());                   
-                    }catch(EOFException e)
-                        {}
-                }
-                System.out.println();             
-                break;
-                
-            case 2:
-                System.out.println();  
-                moyenne= 0;   
-                donnee.seek(0);                    
-                for(int i=0; i < compteur; i++) {    
-                    try{
-                    	donnee.readInt();
-                        donnee.readChar();
-                        donnee.readUTF();
-                        donnee.readChar();
-                        donnee.readUTF();
-                        donnee.readChar();
-                        moyenne += donnee.readDouble();
-                        donnee.readChar();                 
-                    }catch(EOFException e)
-                        {}
-                }
-                System.out.println("La moyenne des salaires est de : "+(moyenne/compteur));                                        
-                System.out.println();             
-                break;
-
-            case 3:
-                System.out.println();                          
-                donnee.seek(donnee.length());
-                compteur+=1;                  
-                try{
-                	donnee.writeInt(compteur);
-                    donnee.writeChar(' ');
-                    System.out.println("Entrez le nom du nouvel employe");
-                    donnee.writeUTF(in.readLine());
-                    donnee.writeChar(' ');
-                    System.out.println("Entrez le prenom du nouvel employe");
-                    donnee.writeUTF(in.readLine());
-                    donnee.writeChar(' ');
-                    System.out.println("Entrez le salaire du nouvel employe");
-                    donnee.writeDouble(Double.parseDouble(in.readLine()));
-                    donnee.writeChar('\n');                    
-                }catch(EOFException e)
-                    {}                     
-                System.out.println();             
-                break;
-
-            case 4 :
-                System.out.println();  
-                donnee.seek(0);
-                do{
-                    System.out.println("Entrez le numéro de l'employé a qui vous voulez"+
-                                       " change le salaire");
-                    numero= Integer.parseInt(in.readLine());
-                }while(numero < 0 || numero > compteur);                 
-                System.out.println("Entrez le montant du nouveau salaire");
-                newSalaire= Double.parseDouble(in.readLine());                   
-                int numDonnee= 0;                   
-                try{
-                    for(int i=0; i < compteur; i++) {
-                        numDonnee= donnee.readInt();                                    
-                        if(numero == numDonnee) {
-                            donnee.readChar();
-                            donnee.readUTF();
-                            donnee.readChar();
-                            donnee.readUTF();
-                            donnee.readChar();
-                            donnee.writeDouble(newSalaire);
-                            donnee.readChar(); 
-                        }else{
-                            donnee.readChar();
-                            donnee.readUTF();
-                            donnee.readChar();
-                            donnee.readUTF();
-                            donnee.readChar();
-                            donnee.readDouble();
-                            donnee.readChar();                             
-                        }             
-                    }
-                }catch(EOFException e)
-                    {}                             
-                System.out.println();              
-                break;
-            
-            case 5:
-                System.out.println();  
-                sortie= true;              
-                break; 
-    	}
-    }while(sortie != true);
-
-    donnee.close();
-    System.exit(0);    
-*/
